e93ea9577d09bcf022d62049a31fcfd2bb1315b6
Innocent1312 <anthonydev33@gmail.com>
2020-01-09 17:22:15 +0200

    Updates

---
+++ precache-manifest.bd21fc3d8ae596eefa2e08e8f6ed77ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c89f7f557588a89e07417c33c0818d14",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4bd41e8f9508020fbb45",
+    "url": "./static/css/main.53b8adf9.chunk.css"
+  },
+  {
+    "revision": "71fa5b579223e7653f96",
+    "url": "./static/js/2.a5703278.chunk.js"
+  },
+  {
+    "revision": "4bd41e8f9508020fbb45",
+    "url": "./static/js/main.d6687f95.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
